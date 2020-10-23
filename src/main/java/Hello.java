a0262cfe5f2e160aec9ea9f28f303407f6680a14
yang <953947710@qq.com>
2020-10-23 12:01:18 +0800

    hello

---
+++ src/main/java/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * @Description:
+ * @Author: YGY
+ * @Date: 2020/10/23
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("say hello");
+    }
+}
